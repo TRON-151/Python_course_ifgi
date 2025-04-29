2092fce799945aa9764ab77991050089f4d3de42
TRON-151 <ousamabinz151@gmail.com>
2025-04-29 08:51:31 +0200

    Task_1_calculator_done

---
+++ exercise_3/go_shopping.py
@@ -0,0 +1,7 @@
+from easy_shopping import calculator
+
+print(calculator.add(7,5))
+print(calculator.sub(34,21))
+print(calculator.multiply(54,2))
+print(calculator.divide(144,2))
+print(calculator.divide(45,0))
